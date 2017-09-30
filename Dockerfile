ae335f37eac7353db1b952c5cf289284125211db
Yogesh Tripathi <Yogesh.Tripathio@macquarie.com>
2017-09-30 14:42:28 +1000

    first

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Base this docker container off the official golang docker image.
+# Docker containers inherit everything from their base.
+FROM golang:1.9
+USER nobody
+RUN mkdir -p /go/src/client
+WORKDIR /go/src/client
+COPY . /go/src/client`
+RUN go-wrapper download && go-wrapper install
+CMD ["main"]
+ENV PORT 9090
+EXPOSE 9090
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)       // set router
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
